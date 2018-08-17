46179b6f774b5b416a7326e326022c52f3331cff
estidea <nick.whatsoever@gmail.com>
2018-08-17 20:48:51 +0300

    update: added slug to CategoryDb Schema

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -33,6 +33,11 @@ class Category
      */
     private $articles;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->articles = new ArrayCollection();
@@ -94,6 +99,18 @@ class Category
                 $article->setCategory(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
